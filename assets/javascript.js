3cc6764b452585b683dcef5b610e9fed50f15d30
Chad Courtney <Chadcourtney567@gmail.com>
2021-01-30 09:59:10 -0500

    displays current weather

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,14 +1,30 @@
+$(document).ready(function () {
 
-const apiKey = "fa2c2f08808de5099f1a217c47174ced";
-let daily = "https://api.openweathermap.org/data/2.5/weather?q=boston&appid=" + apiKey + "&units=imperial";
-let fiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=boston&appid=" + apiKey + "&units=imperial";
-let uvIn = "https://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid=";
+    const apiKey = "fa2c2f08808de5099f1a217c47174ced";
+    // let daily = "https://api.openweathermap.org/data/2.5/weather?q=&appid=" + apiKey + "&units=imperial";
+    // let fiveDay = "https://api.openweathermap.org/data/2.5/forecast?q= &appid=" + apiKey + "&units=imperial";
+    let uvIn = "https://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid=";
+    const recentSearche$ = $(".recentSearches")
+    const cityHeader$ = $(".cityEntered");
+    const currentWIMG$ = $(".currentWeatherImg");
+    const temp$ = $(".temperature");
+    const humid$ = $(".humidity");
+    const windSpeed$ = $(".windSpeed");
 
-$.get(daily).then(function (response) {
-    $(".cityEntered").html(response.name + "Weather Details");
-    $(".temperature").text("Temperature: " + response.main.temp_min);
-    $(".humidity").text("Humididty: " + response.main.humidity);
-    $(".windSpeed").text("Wind Speed: " + response.wind.speed);
-    $(".uvIndex").text("UV Index: " + "")
-});
+    $(".searchBtn").on("click", function () {
+        let cityName = $("input").val();
+        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + apiKey;
+        $.get(queryURL)
+            .then(function (response) {
+                console.log(response);
+                cityHeader$.text(response.name + " ");
+                currentWIMG$.src = "hhtps://openweathermap.org/img/wn" + response.weather[0].icon + ".png"
+                temp$.text("Temperature: " + response.main.temp)
+                humid$.text("Humidity: " + response.main.humidity)
+                windSpeed$.text("Wind Speed: " + response.wind.speed);
+            });
 
+
+
+    });
+})
