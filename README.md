54d0413b15f9cac9ab8b5cfd55c0a7de89d753e5
Chad Courtney <Chadcourtney567@gmail.com>
2021-01-30 19:00:10 -0500

    added 5 day, and uv index

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # 5-Day-Forecast
+
+### This last week i was tasked with creating a 5 Day Forecast. For this task I was asked to put the current weather, and the following 5 days after. Each one displays the temperature, humidity and the date above. The current date also shows the uv index at that location for that day.
+
+<a href="https://chadcourtney9.github.io/Daily-Planner/">See deployed page here!</a>
+
+# Preview
+
+![img of preview]()
+
+# Lighthouse
+
+![img of lighthouse results]()
+
+### Licenses 
+
+Copyright (c) [2020] [Chad Courtney]
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- assets/javascript.js
+++ assets/javascript.js
@@ -1,30 +1,53 @@
 $(document).ready(function () {
 
     const apiKey = "fa2c2f08808de5099f1a217c47174ced";
-    // let daily = "https://api.openweathermap.org/data/2.5/weather?q=&appid=" + apiKey + "&units=imperial";
-    // let fiveDay = "https://api.openweathermap.org/data/2.5/forecast?q= &appid=" + apiKey + "&units=imperial";
-    let uvIn = "https://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid=";
-    const recentSearche$ = $(".recentSearches")
     const cityHeader$ = $(".cityEntered");
     const currentWIMG$ = $(".currentWeatherImg");
     const temp$ = $(".temperature");
     const humid$ = $(".humidity");
     const windSpeed$ = $(".windSpeed");
+    const currentDate$ = $(".currentDate");
+    const uvIndex$ = $(".uvIndex");
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    today = mm + '/' + dd + '/' + yyyy;
 
     $(".searchBtn").on("click", function () {
         let cityName = $("input").val();
         let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + apiKey;
         $.get(queryURL)
             .then(function (response) {
-                console.log(response);
+                currentDate$.text(today)
                 cityHeader$.text(response.name + " ");
-                currentWIMG$.src = "hhtps://openweathermap.org/img/wn" + response.weather[0].icon + ".png"
-                temp$.text("Temperature: " + response.main.temp)
-                humid$.text("Humidity: " + response.main.humidity)
+                currentWIMG$.attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
+                temp$.text("Temperature: " + response.main.temp);
+                humid$.text("Humidity: " + response.main.humidity);
                 windSpeed$.text("Wind Speed: " + response.wind.speed);
+
+                let ultraUrl = `https://api.openweathermap.org/data/2.5/uvi?appid=fa2c2f08808de5099f1a217c47174ced&lat=${response.coord.lat}&lon=${response.coord.lon}`;
+                $.get(ultraUrl)
+                    .then(function (response) {
+                        uvIndex$.text("UV Index: " + response.value);
+                    });
             });
 
 
 
+        let queryFive = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + apiKey;
+        $.get(queryFive)
+            .then(function (data) {
+                for (let i = 1; i < 8; i++) {
+                    let futureDay$ = $(`<div class="card col-2 day${i}"></div>`);
+                    let futureIcon$ = $(`<img class="icon${i}" src="http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png">`);
+                    let futureTemp$ = $(`<p class="${i}">Temperature: ${data.list[i].main.temp}</p>`);
+                    let futurehumid$ = $(`<p class="humidity${i}">Humidity: ${data.list[i].main.humidity}</p>`);
+                    let dateTime = $(`<p class="date${i}"> ${data.list[i * 7].dt_txt}</p>`);
+                    $(".future").append(futureDay$)
+                    futureDay$.append(dateTime, futureIcon$, futureTemp$, futurehumid$)
+                }
+            });
     });
-})
+});
